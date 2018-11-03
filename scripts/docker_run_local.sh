7154430a92e226c283fedb6612bc9330d9ae2745
Arthur Deygin <arthurdgn@gmail.com>
2018-11-03 23:56:45 +1100

    Minor.

--- scripts/docker_run_local.sh
+++ scripts/docker_run_local.sh
@@ -2,4 +2,4 @@
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $HERE/..
 docker build -t movies_notifier .
-docker run --rm -v $HERE/..:/movies_notifier movies_notifier "$@"
+docker run --rm -v $HERE/../data:/movies_notifier/data movies_notifier "$@"
